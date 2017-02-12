aedce03d8f8b7eb39b2789446db8e05a8c56be70
Victor Scopel <victorscopel@hotmail.com>
2017-02-12 14:44:42 -0300

    Update converter.user.js

--- converter.user.js
+++ converter.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/victorscopel/wow-token-converter
 // @homepage https://github.com/victorscopel/wow-token-converter
 // @supportURL https://github.com/victorscopel/wow-token-converter/issues
-// @version      1.1
+// @version      1.2
 // @description  Returns the price and quantity in tokens for every product
 // @author       Victor Scopel
 // @match        https://*.battle.net/shop/*
@@ -17,34 +17,47 @@
 
 (function() {
     'use strict';
-$.ajax ( {
-    type:       'GET',
-    url:        'https://wowtoken.info/snapshot.json',
-    dataType:   'JSON',
-    success:    function (apiJson) {
-        var url = window.location.href;
-        
-        if (url.search("us") >= 0) {
-         var resultObj = apiJson.NA['raw'];
-         var precotoken = 15;
-         if ((url.search("pt") >= 0))
-             var precotoken = 24;
-        }
-        else if (url.search("eu") >= 0){
-         var resultObj = apiJson.EU['raw'];
-         var precotoken = 13;
-        }            
-        
-        var valortoken = resultObj.buy;
-        $(".product-card-container li").each(function( index ) {
-            var valor = $(this).find(".product-price").html().replace(',','.').replace(/[^\d.-]/g, '');
-            var valorfinal = Math.ceil((valor/precotoken))*valortoken;
-            if (valorfinal){
-                $(this).find(".product-card-info").append('<p style="color:#FFD700;position:absolute;font-size: 13px;top:-1px;left:15px;">Gold: '+valorfinal.toLocaleString("us")+'</p>');
-                $(this).find(".product-card-info").append('<p style="color:red;position:absolute;font-size: 13px;top:0;left:unset;right:15px;">Tokens: '+Math.ceil(valorfinal/valortoken)+'</p>');
+    $.ajax ( {
+        type:       'GET',
+        url:        'https://wowtoken.info/snapshot.json',
+        dataType:   'JSON',
+        success:    function (apiJson) {
+            var currency = Msg.userCurrency;
+            var region = Core.region;
+            var tokenprice = 15; //The default value for the token is for dolar
+            if (region == "us") {
+                var resultObj = apiJson.NA.raw;
+                if (currency == "BRL")
+                    tokenprice = 24;
+                if (currency == "AUD")
+                    tokenprice = 17;
+            } else if (region == "eu") {
+                var resultObj = apiJson.NA.raw;
+                if (currency == "EUR")
+                    tokenprice = 13;
+                if (currency == "GBP")
+                    tokenprice = 10;
+            } else if (region == "kr") {
+                console.log("This region isn't suported yet");
+
+            } else if (region == "tw") {
+                console.log("This region isn't suported yet");
+
+            } else if (region == "cn") {
+                console.log("This region isn't suported yet");
             }
-        });
-    }
-} );
-    
+
+
+            var tokengold = resultObj.buy;
+            $(".product-card-container li").each(function( index ) {
+                var productprice = $(this).find(".product-price").html().replace(',','.').replace(/[^\d.-]/g, '');
+                var goldfinal = Math.ceil((productprice/tokenprice))*tokengold;
+                if (goldfinal){
+                    $(this).find(".product-card-info").append('<p style="color:#FFD700;position:absolute;font-size: 13px;top:-1px;left:15px;">Gold: '+goldfinal.toLocaleString("us")+'</p>');
+                    $(this).find(".product-card-info").append('<p style="color:red;position:absolute;font-size: 13px;top:0;left:unset;right:15px;">Tokens: '+Math.ceil(goldfinal/tokengold)+'</p>');
+                }
+            });
+        }
+    } );
+
 })();
